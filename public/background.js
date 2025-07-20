a0c9648cc6fb741b7d2187e862aa82ced95d0a48
qsekhar <subhra.php@gmail.com>
2025-07-20 12:41:36 +0530

    Enable side panel and action click for extension
    Added side panel support to manifest and background script to open the side panel when the extension icon is clicked. Updated permissions and default action title in manifest for improved user experience.

--- public/background.js
+++ public/background.js
@@ -1,2 +1,7 @@
 // Chrome extension background script
 // ...existing code...
+chrome.action.onClicked.addListener(function(tab) {
+  if (tab.id !== undefined) {
+    chrome.sidePanel.open({ tabId: tab.id });
+  }
+});
